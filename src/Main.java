2f36ee8321ced2f5df2e6ec8339ebb2ab3e43fb8
Fernando Calisto <fernandocalisto27@gmail.com>
2025-03-31 15:18:27 -0300

    feat(operações): adicionar, remover e consultar saldo.

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,21 @@ public class Main {
         Scanner leitura = new Scanner(System.in);
         double saldo = 2500;
         int operacao;
-        while (true){
-            System.out.printf("""
-                    ********************************************
+
+        System.out.printf("""
+                ********************************************
                     Dados Do Cliente:
                     
                     Nome: João Muito Pobre Da Silva
                     Tipo de Conta: Corrente
-                    Saldo Inicial: R$%.2f
-                    ********************************************
-                    
+                    Saldo: R$%.2f
+                ********************************************
+                
+                """,saldo);
+
+
+        while (true){
+            System.out.printf("""
                     Operações
                     
                     1 - Consultar Saldos
@@ -23,6 +28,27 @@ public class Main {
                     
                     Digite a opção desejada: 
                     """,saldo);
+
+            operacao = leitura.nextInt();
+
+
+            if (operacao == 1){
+                System.out.printf("Saldo: R$%.2f%n",saldo);
+            }
+            else if (operacao == 2){
+                System.out.print("Insira o valor a receber: ");
+                saldo += leitura.nextDouble();
+            }
+            else if (operacao == 3){
+                System.out.print("Insira o valor a transferir: ");
+                saldo -= leitura.nextDouble();
+            }
+            else if (operacao == 4){
+                System.out.println("Ok, Até a próxima!");
+                break;
+            }
+            else{
+                System.out.println("Operação Inválida!!!");
             }
         }
     }
